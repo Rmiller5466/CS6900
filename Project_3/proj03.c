a6566f1fcf6a597ae6ecc60a41039b122ddea609
Ryan Miller <>
2021-02-10 00:19:27 -0500

    First draft of Rank 0 Logic

--- Project_3/proj03.c
+++ Project_3/proj03.c
@@ -1,15 +1,14 @@
-//
-// cs4900
+// cs6900
 // Project 03
-// Dr. John Nehrbass
-// w006jwn
+// Ryan Miller
+// File Template Provided by: Dr. John Nehrbass
+// w051rem
 // Due 12 Feb 2021
 // System = bender
 // Compiler syntax = ./compile.sh proj03
 // Job Control File = proj03.sbatch
 // Additional File  = NA
 // Results file     = proj03.txt
-//
 
 #include <mpi.h>
 #include <stdio.h>
@@ -62,10 +61,10 @@ unsigned long* calMandP(int N ) {
     printf("%d x=%f y=%f z=%f rs=%f\n",randomPnts,x,y,z,rs);
 #endif 
 
-    if (rs<1){                   // or should it be rs<=1
+    if (rs<1){                  
       // Inside circle
       ++M;
-      if (x<rsq3 && y<rsq3 && z<rsq3){     // or should it be <=
+      if (x<rsq3 && y<rsq3 && z<rsq3){ 
 	// inside inner square
 	++P;
       }
@@ -79,6 +78,8 @@ unsigned long* calMandP(int N ) {
 
 
 int main (int argc, char *argv[]){
+  double start_time = time(NULL);
+
   MPI_Init(NULL, NULL);
 
   int world_size;
@@ -91,42 +92,71 @@ int main (int argc, char *argv[]){
 
   int name_len;
   MPI_Get_processor_name(processor_name, &name_len);
-
-
-
-  // Use current time as seed for random generator 
-  srand(time(NULL)*(double)world_rank); 
-  
+   
   // For convenience I passed "N" on the command line
   // Not checking for valid input
   char *a = argv[1];
   int N = atoi(a);
-  
+ 
   // The Max value of unsigned long is 18,446,744,073,709,551,615
-  unsigned long* MnP;   // Store in vector M=MnP[0] and P=MnP[1]
-  
-  // Return M and P for this case
-  MnP=calMandP(N);
-  
-  // Results
-   
-  double sqrt3=1.73205080757;
-  double p1=6.0*((double)MnP[0]/(double)N);
-  // double p2 = 3.0*((double)MnP[0]/(double)MnP[1]);
-  double p2=(2.0*(((double)MnP[0]/(double)MnP[1])/sqrt3));
-  // double p1=(2.0*((double)MnP[0]/(double)N))/sqrt3;
-	     printf("Reporting From: Processor %s, Rank %d Of %d\nM=%llu P=%llu\nPi1: %f Pi2: %f\n",  processor_name, world_rank, world_size, MnP[0], MnP[1],p1,p2);
-  // printf(,p1,p2);
+  unsigned long* MnP,MnP_temp,MnP_total;   // Store in vector M=MnP[0] and P=MnP[1]
 
-  MPI_Finalize();// Finalize the MPI environment.
+  if (world_rank == 0){
+    
+    double sqrt3=1.73205080757;
+    double epsilon = 0.001;
+    int MAX_RUNS = 1000;
+    int current_runs = 0;
+    double cube2sphere = 0, sphere2cube = 0, est_value1 = 0, est_value2 = 0;
+    int finished_1 = 0, finished_2 = 0;
+
+    while (current_runs < MAX_RUNS){
+      
+      if (finished_1 == 1 && finished_2 == 1) {
+	//TODO: Signal to stop
+      }
+      
+      MPI_Barrier(MPI_COMM_WORLD);
+      MPI_Reduce(MnP,MnP_temp,2,MPI_UNSIGNED_LONG,MPI_SUM,0,MPI_COMM_WORLD);
+      
+      MnP_total[0] += MnP_temp[0];
+      MnP_total[1] += MnP_temp[1];
+      
+      if (finished_1 == 0){
+	
+	cube2sphere=6.0*((double)MnP_total[0]/(double)N);
+	if ((cube2sphere - est_value1) > epsilon) || (( est_value1 - cube2sphere) > epsilon) {
+	    est_value1 = cube2sphere;
+	  }else{
+	  finished_1  = 1;
+	}
+      }
+      
+
+      if (finished_2 == 0){
+	
+	sphere2cube=(2.0*(((double)MnP_total[0]/(double)MnP_total[1])/sqrt3));
+	if ((sphere2cube - est_value2) > epsilon) || (( est_value2 - sphere2cube) > epsilon) {
+	    est_value2 = sphere2cube;
+	  }else{
+	  finished_2  = 1;
+	}
+      }
+    }
+    
+    //TODO: Finalize and print Final Results
+
+
+  }else{
+    
+    // Use current time as seed for random generator
+    srand(time(NULL)*(double)world_rank);
+    
+    //TODO: Set up M and P and return to rank 0
+    //  MnP=calMandP(N);
+  }
+  
+  //USE LATER:
+  //  printf("Reporting From: Processor %s, Rank %d Of %d\nM=%llu P=%llu\nPi1: %f Pi2: %f\n",  processor_name, world_rank, world_size, MnP[0], MnP[1],p1,p2);
+  
 }
-
-// Get the number of processes
-// Get the rank of the process
-
-// Get the name of the processor 
-
-//printf("Hello world from processor %s, rank %d out of %d processors\n",  processor_name, world_rank,  world_size);
-
-
-
