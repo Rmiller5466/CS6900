07bca66fea57591048ed9e663d9a227ada5e425e
Ryan Miller <>
2021-03-14 22:39:55 -0400

    Finished Scalar GE and BS - Proj5

--- Project_5/proj05S.c
+++ Project_5/proj05S.c
@@ -5,14 +5,15 @@
 
 // Flag 0: Random 1: Diagonal 2: Filename
 // filename default is 0
-void createMatrix(int N, double** mspace, int flag, char* filename){ 
+void createMatrix(int N, double** mspace, double* ref, int flag, char* filename){ 
 
   int i, j;
+  double sum; 
 
   if (flag == 0) {
     for (i = 0; i < N; i++) {
       for (j = 0; j < N; j++) {
-	mspace[i][j] = (double)(rand() % 50);  
+	mspace[i][j] = (double)(rand() % 100);  
       }
     }
   }else if (flag == 1){
@@ -26,20 +27,30 @@ void createMatrix(int N, double** mspace, int flag, char* filename){
       }
     }
   }else {
-    mspace[0][0] = 1;
-    mspace[0][1] = 2;
-    mspace[0][2] = 3;
-    mspace[1][0] = 2;
-    mspace[1][1] = -1;
-    mspace[1][2] = 1;
-    mspace[2][0] = 3;
-    mspace[2][1] = 0;
-    mspace[2][2] = -1;
+    mspace[0][0] = 2;
+    mspace[0][1] = 3;
+    mspace[0][2] = 1;
+    mspace[0][3] = -1;
+    mspace[1][0] = 6;
+    mspace[1][1] = 0;
+    mspace[1][2] = 15;
+    mspace[1][3] = -6;
+    mspace[2][0] = 4;
+    mspace[2][1] = 3;
+    mspace[2][2] = 5;
+    mspace[2][3] = -4;
+    mspace[3][0] = 8;
+    mspace[3][1] = 18;
+    mspace[3][2] = -2;
+    mspace[3][3] = 3;
+  }
 
-
-
-
-
+  for (i = 0; i < N; i++){
+    sum = 0;
+    for (j = 0; j < N; j++){
+      sum += mspace[i][j] * ref[j];
+    }
+    mspace[i][N] = sum;
   }
 }
 
@@ -48,13 +59,26 @@ void printMat(double** mat, int N){
 
   printf("Matrix %d\n", N);
   for (i = 0; i < N; i++) {
-    for (j = 0; j < N; j++) {
-      if (j == (N - 1)){
-	printf("%.2f\n", mat[i][j]);
+    for (j = 0; j < N+1; j++) {
+      if (j == (N)){
+	printf("%.3f\n", mat[i][j]);
       }else{
-	printf("%.2f ", mat[i][j]);
+	printf("%.3f ", mat[i][j]);
       }
     }
+  }
+}
+
+void printVec(double* vec, int N){
+  int i;
+
+  printf("Vector %d\n", N);
+  for (i = 0; i < N; i++) {
+      if (i == (N - 1)){
+	printf("%.3f\n", vec[i]);
+      }else{
+	printf("%.3f ", vec[i]);
+      }
   }
 }
 
@@ -66,31 +90,37 @@ void GE(double** mat, int N){
   double cancelVal;
 
   for (base = 0; base < N-1; base++) {
-    /* maxVal = mat[base][base]; */
-    /* maxIndex = base; */
-    /* for (swap = base; swap < N; swap++){ */
-    /*   if (mat[swap][base] > maxVal){ */
-    /* 	maxVal = mat[swap][base]; */
-    /* 	maxIndex = swap; */
-    /*   } */
-    /* } */
-    /* if (maxIndex != base){ */
-    /*   temp = mat[base]; */
-    /*   mat[base] = mat[maxIndex]; */
-    /*   mat[maxIndex] = temp; */
-    /* } */
-
-    /* for (j = i + 1; j < N-1; j++) { */
-    /*   temp = mat[j][i]/mat[i][i]; */
-    /*   for (k = i+1; j < N-1; j++) { */
-    /* 	printf("I: %d, J: %d, K: %d\n",i,j,k); */
-    /* 	mat[j][k] = ( mat[j][k] - (temp * mat[i][k])); */
-    /*   }  */
-    /* } */
-    for (row = base + 1; row < N; row++){
-      for (col = 0; col < N; col++){
-	mat[row][col] = mat[row][col] + (-1 * (mat[row][base]/mat[base][base]) * mat[base][col]);
+    maxVal = fabs(mat[base][base]);
+    maxIndex = base;
+    for (swap = base; swap < N; swap++){
+      if (fabs(mat[swap][base]) > maxVal){
+    	maxVal = fabs(mat[swap][base]);
+    	maxIndex = swap;
       }
+    }
+    if (maxIndex != base){
+      temp = mat[base];
+      mat[base] = mat[maxIndex];
+      mat[maxIndex] = temp;
+    }
+
+    for (row = base + 1; row < N; row++){
+      cancelVal = mat[row][base]/mat[base][base];
+      for (col = 0; col < N+1; col++){
+	mat[row][col] = mat[row][col] + (-1 * cancelVal  * mat[base][col]);
+      }
+    }
+  }
+}
+
+void BS(double** mat, double* xVec, int N){
+  int i, j;
+  
+  for ( i = N-1; i >= 0; i--){
+    xVec[i] = mat[i][N] / mat[i][i];
+    for (j = 0; j < i; j++){
+      mat[j][N] = mat[j][N] - xVec[i] * mat[j][i];
+      mat[j][i] = 0;
     }
   }
 }
@@ -110,22 +140,48 @@ double main(double argc, char** argv) {
   }
 
   char* nu = 'a';
-  int N = 6;
+  int N = 4;
   double* space1[N];
+  double* ref1 = (double *)malloc(sizeof(double) * N);
+  double* x1 = (double *)malloc(sizeof(double) * N);
+  assert(ref1 != NULL);
+  assert(x1 != NULL);
   int i, j;
 
   for (i = 0; i < N; i++){
-    space1[i] = (double *)malloc(sizeof(double) * N);
+    space1[i] = (double *)malloc(sizeof(double) * (N + 1));
     assert(space1[i] != NULL);
   }
 
- 
-  createMatrix(N, space1, 0, nu);
-  printMat(space1, N);
-  GE(space1, N);
-  printMat(space1, N);
   for (i = 0; i < N; i++) {
-    free(space1[i]);
+    ref1[i] = (double)(rand() % 15);  
   }
 
+  /* ref1[0] = -27.0/2.0; */
+  /* ref1[1] = 26.0/3.0; */
+  /* ref1[2] = 7; */
+  /* ref1[3] = 2; */
+
+  printf("Reference Vector: \n");
+  printVec(ref1, N);
+  createMatrix(N, space1, ref1, 0, nu);
+  printMat(space1, N);
+
+  
+  GE(space1, N);
+  printf("After GE\n");
+  printMat(space1, N);
+
+  BS(space1, x1, N);
+  printf("After BS\n");
+  printf("X Vector:\n");
+  printVec(x1, N);
+  printMat(space1, N);
+
+
+  for (i = 0; i < N; i++) {
+    free(space1[i]);
+    free(ref1);
+    free(x1);
+  }
 }
